328096f32253f31aff4c514193b2700ba616dd38
David Souza <dsouza@apple.com>
2013-04-30 12:17:46 -0700

    Added completion for tasks with passing tests

--- spec/features/user_manages_tasks_spec.rb
+++ spec/features/user_manages_tasks_spec.rb
@@ -20,4 +20,18 @@ feature 'User manages tasks' do
 
     expect(page).to have_content task_name
   end
+
+  scenario 'mark task completed' do
+    task_name = 'Mow the lawn'
+    visit root_path
+    click_link 'New Task'
+    fill_in 'Name', with: task_name
+    click_button 'Create Task'
+
+    within "td:contains('#{task_name}')" do
+      click_link "Complete"
+    end
+
+    expect(page).to have_css 'td.completed', text: task_name
+  end
 end
